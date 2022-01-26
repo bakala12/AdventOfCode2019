e392e85ff69048e610647c051c3d83d8574ebe0f
Mateusz Bąkała <maba@netcompany.com>
2022-01-26 16:16:08 +0100

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+            Part1(numbers);
+            Part2(numbers);
+        }
+
+        private static void Part1(int[] numbers)
+        {
+            Console.WriteLine(numbers.Sum(n => Math.Max(n / 3 - 2, 0)));
+        }
+
+        private static void Part2(int[] numbers)
+        {
+            Console.WriteLine(numbers.Sum(n => CalculateFuel(n)));
+        }
+
+        private static int CalculateFuel(int mass)
+        {
+            if(mass <= 0) return 0;
+            var fuel = Math.Max(mass / 3 - 2, 0);
+            return fuel + CalculateFuel(fuel);
+        }
+    }
+}
